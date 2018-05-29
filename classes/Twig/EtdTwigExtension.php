20277aaf0f8d5b057c74643bb670c987a1b5aa7f
Jean-Baptiste Alleaume <jbanety@gmail.com>
2018-05-29 09:28:39 +0200

    add minify

--- classes/Twig/EtdTwigExtension.php
+++ classes/Twig/EtdTwigExtension.php
@@ -8,6 +8,7 @@ class EtdTwigExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFilter('ampUrl', [$this, 'ampUrl'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFilter('minify', [$this, 'minify'], ['is_safe' => ['all']]),
         ];
     }
 
@@ -21,6 +22,15 @@ class EtdTwigExtension extends \Twig_Extension
         $amp->modifyHeader('append_url_extension', $extension);
 
         return $amp->canonical();
+    }
+
+    public function minify($buffer, $type = 'css') {
+
+        $minifier = ($type == 'css') ? new \MatthiasMullie\Minify\CSS() : new \MatthiasMullie\Minify\JS();
+        $minifier->add($buffer);
+
+        return $minifier->minify();
+
     }
 
 }
